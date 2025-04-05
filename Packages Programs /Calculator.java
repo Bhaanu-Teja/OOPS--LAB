e93063280bbf36142bca24dc5c9f53a4ff7b81b2
Bhaanu Teja <bhaanuteja999@gmail.com>
2025-04-05 20:52:31 +0530

    Update and rename FileAnalyzer.java to Calculator.java

---
+++ Packages Programs /Calculator.java
@@ -0,0 +1,37 @@
+package mypackage.calculator; 
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b != 0)
+            return a / b;
+        else {
+            System.out.println("Cannot divide by zero!");
+            return 0;
+        }
+    }
+}
+
+import mypackage.calculator.Calculator;
+
+public class MainCalculator {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
+
